fe25e1b473e009143e506069d803f03cde9288af
Mar1cX <marisrucko@gmail.com>
2021-07-16 01:56:39 +0300

    Added params: autocomplete functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,94 @@
 import * as vscode from 'vscode';
+import { parse, stringify } from 'yaml';
 
 export function activate(context: vscode.ExtensionContext) {
-		console.log('Congratulations, your extension "kedro-toolkit" is now active!');
 
-	context.subscriptions.push(vscode.commands.registerCommand('kedro-toolkit.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello from Kedro Toolkit!');
-	}));
+  const paramsAutocomplete = vscode.languages.registerCompletionItemProvider('python', {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+      const nonQuotesParamsItem = new vscode.CompletionItem('params');
+      nonQuotesParamsItem.kind = vscode.CompletionItemKind.Keyword;
+      nonQuotesParamsItem.commitCharacters = [':'];
+      nonQuotesParamsItem.documentation = new vscode.MarkdownString('Press `:` to get `params:`');
 
-	context.subscriptions.push(vscode.commands.registerCommand('kedro-toolkit.askQuestion', async() => {
-		const answer = await vscode.window.showInformationMessage('How are your day?', 'good', 'bad');
+      return [
+        nonQuotesParamsItem
+      ];
+    }
+  });
 
-		if (answer === 'bad') {
-			vscode.window.showInformationMessage("That's too bad");
-		} else {
-			console.log({ answer });
-		}
-	}));
+  const configAutocomplete = vscode.languages.registerCompletionItemProvider('yaml', {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+      const configItem = new vscode.CompletionItem('type');
+      configItem.kind = vscode.CompletionItemKind.Keyword;
+      configItem.commitCharacters = [':'];
+      configItem.documentation = new vscode.MarkdownString('Press `:` to get `type:`');
+
+      return [
+        configItem,
+      ];
+    }
+  });
+
+  const mapParametersListByFile = async(file: vscode.Uri):  Promise<vscode.CompletionItem[]> => {
+    try {
+      const document = await vscode.workspace.openTextDocument(file.path);
+
+      return Object.keys(parse(document.getText())).map(keyValue => {
+        return new vscode.CompletionItem(keyValue, vscode.CompletionItemKind.Method);
+      });
+    } catch (err) {
+      console.log(err);
+
+      return [];
+    }
+  };
+
+  const retrieveDefinedParametersList = async(): Promise<vscode.CompletionItem[]> => {
+    try {
+      const config = vscode.workspace.getConfiguration('findParametersFiles');
+      const files = await vscode.workspace.findFiles(<string>config.get('parametersFileIncludeGlob'), <string>config.get('fileExcludeGlob'), <number>config.get('maximumFilesAmount'));
+      const promiseParameters = files.map(file => mapParametersListByFile(file));
+
+      return Promise.all(promiseParameters).then((parameters) => {
+        return ([] as vscode.CompletionItem[]).concat.apply([], parameters);
+      }).catch(() => {
+        console.error;
+        return [];
+      });
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  };
+
+  const paramsListAutocomplete = vscode.languages.registerCompletionItemProvider('python', {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+        const linePrefix = document.lineAt(position).text.substr(0, position.character);
+
+        if (!linePrefix.endsWith('params:')) {
+          return undefined;
+        }
+
+        return retrieveDefinedParametersList();
+      }
+    },
+    ':'
+  );
+
+	const catalogListAutocomplete = vscode.languages.registerCompletionItemProvider('yaml', {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('type:')) {
+					return undefined;
+				}
+
+				return [
+					new vscode.CompletionItem(' pandas.CSVDataSet', vscode.CompletionItemKind.Method),
+				];
+			}
+		},
+		':' // triggered whenever a 'type:' is being typed
+	);
+
+  context.subscriptions.push(paramsAutocomplete, configAutocomplete, paramsListAutocomplete, catalogListAutocomplete);
 }
-
-export function deactivate() {}
